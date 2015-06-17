535e79f86efe1a6404fc77da5a7094814f7b072f
Ivan Shamatov <status.enable@gmail.com>
2015-06-17 10:44:47 +0300

    first commit

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject api "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [ring "1.4.0-RC1"]
+                 [org.clojure/data.json "0.2.6"]
+                 [org.clojure/java.jdbc "0.3.7"]
+                 [mysql/mysql-connector-java "5.1.6"]])
---
+++ src/api/core.clj
@@ -0,0 +1,26 @@
+(ns api.core)
+
+(require '[clojure.java.jdbc :as db])
+
+(def mysql-db {:classname "com.mysql.jdbc.Driver"
+               :subprotocol "mysql"
+               :subname "//127.0.0.1:3306/passports"
+               :user "root"
+               :password ""})
+
+(defn check-passport
+  [serial number]
+  (db/query mysql-db
+    ["select * from passports where serial = ? and number = ?" serial number]
+    :row-fn :cost)
+  )
+
+
+
+
+(defn handler [request]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "Hello World"})
+
+
